e61a886f6fa63dc7d3abb4012a4f7e4ac4913460
Alex Woods <alex.woods.1404@gmail.com>
2021-07-28 15:37:08 +0100

    sample call

--- alarm/classifier.py
+++ alarm/classifier.py
@@ -9,3 +9,23 @@ interpreter.allocate_tensors()
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+
+image = Image.open('photo.jpg')
+input_data = image.resize((200, 150))
+input_data = np.array(input_data).astype(np.float32)
+
+input_data = (np.expand_dims(input_data,0))
+
+print(input_data.shape)
+# plt.imshow(input_data[0].astype('uint8'))
+start = time.time()
+
+interpreter.set_tensor(input_details[0]['index'], input_data)
+interpreter.invoke()
+end = time.time()
+print(f"took: {(end - start)*1000} ms")
+
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])[0]
+print(output_data)
