1b43aa4dd1a7f8bdc281f10c2d6d9fe2285ef7d5
Alex Woods <alex.woods.1404@gmail.com>
2021-07-28 15:34:35 +0100

    Classifier setup

---
+++ alarm/classifier.py
@@ -0,0 +1,11 @@
+import tflite_runtime.interpreter as tflite
+from PIL import Image
+import numpy as np
+import time
+
+interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
